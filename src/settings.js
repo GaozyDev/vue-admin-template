60aa3ce4eebc6386c93d321c285df6dab237e57a
GaozyDev <670453367@qq.com>
2022-05-12 15:05:10 +0800

    first commit

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 网站的标题
+  title: '后台管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 是否首行冻结
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 侧边栏是否带图标
+  sidebarLogo: false
+}
