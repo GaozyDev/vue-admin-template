0b9c0fd50e78a5a864ec0cbe7097f23f7e0f5009
GaozyDev <670453367@qq.com>
2022-05-14 09:26:49 +0800

    添加品牌

--- src/views/product/TradeMark/tradeMark.vue
+++ src/views/product/TradeMark/tradeMark.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!--按钮-->
-    <el-button type="primary" icon="el-icon-plus" style="margin:10px 0px">添加</el-button>
+    <el-button type="primary" icon="el-icon-plus" style="margin:10px 0px" @click="showDialog">添加</el-button>
     <!--表格组件
     data:表格组件将来需要展示的数据----数组类型
     border：表格边框
@@ -23,7 +23,7 @@
       </el-table-column>
       <el-table-column prop="prop" label="操作" width="width">
         <template slot-scope="{row,$index}">
-          <el-button type="warning" icon="el-icon-edit" size="mini">修改</el-button>
+          <el-button type="warning" icon="el-icon-edit" size="mini" @click="updateTradeMark">修改</el-button>
           <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
         </template>
       </el-table-column>
@@ -50,8 +50,38 @@
       @current-change="getPageList"
       @size-change="handleSizeChange"
       layout="prev,pager,next,jumper,->,sizes,total"
-      >
+    >
     </el-pagination>
+    <!--
+    对话框
+    :visible.sync:控制对话框显示与隐藏
+    -->
+    <el-dialog title="添加品牌" :visible.sync="dialogFormVisible">
+      <el-form style="width: 80%">
+        <el-form-item label="品牌名称" label-width="100px">
+          <el-input autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" label-width="100px">
+        <!--
+
+          -->
+        <el-upload
+          class="avatar-uploader"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload">
+          <img v-if="imageUrl" :src="imageUrl" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -61,13 +91,17 @@ export default {
   data() {
     return {
       //当前第几页
-      page:1,
+      page: 1,
       //每一页展示的数据条数
-      limit:3,
+      limit: 3,
       //总数据数
-      total:0,
+      total: 0,
       //列表展示的数据
-      list:[]
+      list: [],
+      //对话框显示与隐藏
+      dialogFormVisible: false,
+      //上传图片使用的属性
+      imageUrl: ''
     }
   },
   //组件挂载完毕发请求
@@ -80,10 +114,10 @@ export default {
     async getPageList(pager = 1) {
       this.page = pager
       //解构出参数
-      const {page,limit} = this
+      const {page, limit} = this
       //获取品牌列表接口,向服务器发请求需要带参，因此在data中初始化两个字段，代表给服务器传递参数
-      let result = await this.$API.tradeMark.reqTradeMarkList(page,limit)
-      if(result.code==200) {
+      let result = await this.$API.tradeMark.reqTradeMarkList(page, limit)
+      if (result.code == 200) {
         //展示的数据总条数与列表展示的数据
         this.total = result.data.total
         this.list = result.data.records
@@ -93,11 +127,60 @@ export default {
     handleSizeChange(limit) {
       this.limit = limit
       this.getPageList()
+    },
+    //点击添加品牌按钮
+    showDialog() {
+      this.dialogFormVisible = true
+    },
+    //修改某一个品牌
+    updateTradeMark() {
+      this.dialogFormVisible = true
+    },
+    //上传图片相关的回调
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+      return isJPG && isLt2M;
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
 
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>
