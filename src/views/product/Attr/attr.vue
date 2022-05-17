138bae3ee800eda8d69bb4102f20218d6607df99
GaozyDev <670453367@qq.com>
2022-05-17 11:20:05 +0800

    获取平台属性进行展示

--- src/views/product/Attr/attr.vue
+++ src/views/product/Attr/attr.vue
@@ -3,7 +3,27 @@
     <el-card style="margin: 20px 0px">
       <CategorySelect @getCategoryId="getCategoryId"></CategorySelect>
     </el-card>
-    <el-card></el-card>
+    <el-card>
+      <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
+      <!--表格：展示平台属性-->
+      <el-table style="width: 100%" border :data="attrList">
+        <el-table-column type="index" label="序号" width="80" align="center">
+        </el-table-column>
+        <el-table-column prop="attrName" label="属性名称" width="150">
+        </el-table-column>
+        <el-table-column prop="prop" label="属性值列表" width="width">
+          <template slot-scope="{row,$index}">
+            <el-tag type="success" v-for="(attrValue,index) in row.attrValueList" :key="attrValue.id" style="margin: 0px 10px">{{attrValue.valueName}}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="prop" label="操作" width="150">
+          <template slot-scope="{row,$index}">
+            <el-button type="warning" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
@@ -14,7 +34,9 @@ export default {
     return {
       category1Id: '',
       category2Id: '',
-      category3Id: ''
+      category3Id: '',
+      //接收平台属性的字段
+      attrList:[]
     }
   },
   methods: {
@@ -35,8 +57,13 @@ export default {
       }
     },
     //获取平台属性的数据
-    getAttrList() {
-      console.log('faqingqiu')
+    //当用户确定三级分类的id的时候，可以向服务器发请求进行数据展示
+    async getAttrList() {
+      const {category1Id,category2Id,category3Id} = this
+      let result = await this.$API.attr.reqAttrList(category1Id,category2Id,category3Id)
+      if(result.code==200) {
+        this.attrList = result.data
+      }
     }
   }
 }
