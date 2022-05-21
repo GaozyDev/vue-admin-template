fd412724db761eb6bfdd07e438b535432e3f7ad7
GaozyDev <670453367@qq.com>
2022-05-21 19:47:28 +0800

    删除属性值

--- src/views/product/Attr/attr.vue
+++ src/views/product/Attr/attr.vue
@@ -51,12 +51,20 @@
                 @blur="toLook(row)"
                 @keyup.native.enter="toLook(row)"
                 :ref="$index"></el-input>
-              <span v-else @click="toEdit(row,$index)" style="display: block">{{row.valueName}}</span>
+              <span v-else @click="toEdit(row,$index)" style="display: block">{{ row.valueName }}</span>
             </template>
           </el-table-column>
           <el-table-column prop="prop" label="操作" width="width">
             <template slot-scope="{row,$index}">
-              <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+              <!--气泡确认框-->
+              <el-popconfirm :title="`确定删除${row.valueName}？`" @onConfirm="deleteAttrValue($index)">
+                <el-button
+                  type="danger"
+                  icon="el-icon-delete"
+                  size="mini"
+                  slot="reference"
+                ></el-button>
+              </el-popconfirm>
             </template>
           </el-table-column>
         </el-table>
@@ -70,6 +78,7 @@
 <script>
 //按需引入
 import cloneDeep from 'lodash/cloneDeep'
+
 export default {
   name: 'Attr',
   data() {
@@ -128,10 +137,10 @@ export default {
       this.attrInfo.attrValueList.push({
         attrId: this.attrInfo.id,//对于修改某一个属性，可以在已有的属性值上增加新的属性（新增的属性值要把已有的属性id带上）
         attrName: '',
-        flag:true
+        flag: true
       })
-      this.$nextTick(()=>{
-        this.$refs[this.attrInfo.attrValueList.length-1].focus()
+      this.$nextTick(() => {
+        this.$refs[this.attrInfo.attrValueList.length - 1].focus()
       })
     },
     //添加属性按钮的回调
@@ -159,42 +168,47 @@ export default {
       //由于数据结构里面存在对象里面套数组，数组里面套对象，因此需要深拷贝
       this.attrInfo = cloneDeep(row)
       //修改某一个属性，将相应的属性值元素添加flag元素
-      this.attrInfo.attrValueList.forEach(item=>{
+      this.attrInfo.attrValueList.forEach(item => {
         //这样也可以给属性值添加flag字段，但是视图不会跟着改变，因为flag不是响应式数据
         //Vue 无法探测普通的新增 property
         // item.flag = false
         //第一个参数：哪个对象；第二个参数：添加新的响应式属性；第三个参数：新的属性的属性值
-        this.$set(item,'flag',false)
+        this.$set(item, 'flag', false)
       })
     },
     //失去焦点的回调
     //row是用户添加的最新的属性值
     toLook(row) {
       //属性值不能为空，如果为空需要给用户提示
-      if(row.valueName.trim()=='') {
+      if (row.valueName.trim() == '') {
         this.$message('请输入正确的属性值')
         return
       }
       //新增的属性值不能与已有的属性值重复
-      let isRepat = this.attrInfo.attrValueList.some(item=>{
+      let isRepat = this.attrInfo.attrValueList.some(item => {
         //判断的时候将row从数组里面去除
-        if(row!==item) {
+        if (row !== item) {
           return row.valueName == item.valueName
         }
       })
-      if(isRepat) return
+      if (isRepat) return
       //编辑模式变为查看模式
       row.flag = false
     },
     //点击span的回调，变为编辑模式
-    toEdit(row,index) {
+    toEdit(row, index) {
       row.flag = true
       //获取input节点，实现自动聚焦
       //点击span，切换为input模式，对于浏览器而言，页面重绘与重排是耗时间的，不可能点击的一瞬间就立马获取到input
-      this.$nextTick(()=>{
+      this.$nextTick(() => {
         //获取相应的input表单元素实现聚焦
         this.$refs[index].focus()
       })
+    },
+    //气泡确认框确定按钮的回调
+    deleteAttrValue(index) {
+      //当前操作不需要发请求
+      this.attrInfo.attrValueList.splice(index,1)
     }
   }
 }
