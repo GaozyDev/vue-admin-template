d45a89fd36b41366f87ffec9ff1d7d95b468962a
GaozyDev <670453367@qq.com>
2022-05-26 12:15:13 +0800

    折线图完成

--- src/views/dashboard/Card/lineChart/lineChart.vue
+++ src/views/dashboard/Card/lineChart/lineChart.vue
@@ -5,31 +5,72 @@
 
 <script>
 //引入echarts
-import echarts from 'echarts'
+import * as echarts from 'echarts'
+
 export default {
-  name: 'lineChart',
+  name: '',
   mounted() {
     //初始化echarts实例
-    let lineChart = echarts.init(this.$refs.charts)
+    let lineCharts = echarts.init(this.$refs.charts)
     //配置数据
-    lineChart.setOption({
-      xAxis:{},
-      yAxis:{},
+    lineCharts.setOption({
+      xAxis: {
+        //隐藏x轴
+        show: false,
+        type: 'category'
+      },
+      yAxis: {
+        //隐藏y轴
+        show: false
+      },
       //系列
-      series:[
+      series: [
         {
-          type:'line',
-          data:[10,7,45,89,23]
+          type: 'line',
+          data: [10, 7, 45, 89, 23,52,12,32,45],
+          //拐点样式设置
+          itemStyle: {
+            opacity: 0
+          },
+          //线条的样式
+          lineStyle: {
+            color: 'purple'
+          },
+          //填充颜色
+          areaStyle: {
+            color: {
+              type: 'linear',
+              x: 0,
+              y: 0,
+              x2: 0,
+              y2: 1,
+              colorStops: [{
+                offset: 0, color: 'purple' // 0% 处的颜色
+              }, {
+                offset: 1, color: '#fff' // 100% 处的颜色
+              }],
+              global: false // 缺省为 false
+            }
+          },
+          //设置平滑的线
+          smooth:true
         }
-      ]
+      ],
+      //调试布局
+      grid: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
+      }
     })
   }
 }
 </script>
 
 <style scoped>
-  .charts{
-    width: 100%;
-    height: 100%;
-  }
+.charts {
+  width: 100%;
+  height: 100%;
+}
 </style>
