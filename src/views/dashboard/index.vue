24bbfd34b05358f786d84d95b610c289eaef6efd
GaozyDev <670453367@qq.com>
2022-05-27 10:33:47 +0800

    observe静态组件完毕

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,17 +2,20 @@
   <div>
     <card></card>
     <Sale></Sale>
+    <Observe></Observe>
   </div>
 </template>
 
 <script>
+import Observe from "@/views/dashboard/Observe/observe";
 import Sale from "@/views/dashboard/sale/sale";
 import Card from "@/views/dashboard/Card/card";
 export default {
   name: '',
   components:{
     Card,
-    Sale
+    Sale,
+    Observe
   }
 }
 </script>
